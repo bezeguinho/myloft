2d712247e3e1d211563e68069fbdac9fa156caac
bezeguinho <bezeguinho@gmail.com>
2026-04-21 13:45:59 +0100

    a

--- update_schema.py
+++ update_schema.py
@@ -1,10 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from main import app, db
 from sqlalchemy import text
 from datetime import datetime, timedelta
 
 with app.app_context():
     try:
-        db.session.execute(text('ALTER TABLE users ADD COLUMN data_expiracao DATETIME;'))
+        db.session.execute(text('ALTER TABLE users ADD COLUMN data_expiracao TIMESTAMP;'))
         db.session.commit()
         print("Coluna 'data_expiracao' adicionada com sucesso.")
     except Exception as e:
@@ -14,9 +17,11 @@ with app.app_context():
     print("Preenchendo expiração para contas antigas...")
     from main import User
     users = User.query.all()
+    count = 0
     for u in users:
         if not u.data_expiracao:
             # Assumimos 1 ano a partir de agora de borla para contas antigas
             u.data_expiracao = datetime.now() + timedelta(days=365)
+            count += 1
     db.session.commit()
-    print("Tabela populada com sucesso!")
+    print(f"Tabela populada com sucesso! Atualizados {count} utilizadores.")
